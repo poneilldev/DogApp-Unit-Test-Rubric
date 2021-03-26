55932894a2083962baaa8d5fc16a8b63430e9d0a
Paul O'Neill <p.oneill.apc@gmail.com>
2021-03-26 15:38:01 -0600

    Initial project commit

---
+++ Dog-app/Dog-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dog-app
+//
+//  Created by Paul O'Neill on 3/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
